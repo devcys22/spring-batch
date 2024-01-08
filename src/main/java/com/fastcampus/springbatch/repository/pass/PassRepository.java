1bda411737794d307f8d2c99bf6cb3e3f0e6202e
dev22 <ysoli36512@gmail.com>
2024-01-08 20:08:15 +0900

    #3 - 이용기간에 따른 만료

---
+++ src/main/java/com/fastcampus/springbatch/repository/pass/PassRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.springbatch.repository.pass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassRepository extends JpaRepository<PassEntity, Integer> {
+
+}
